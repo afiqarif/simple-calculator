1b20a1034e18ebda940f597f2f05a4c1ea52c8fe
afiqarif <akamarulzaidi@gmail.com>
2025-01-13 20:07:47 +0800

    dssadad

--- calculator-backend/src/main/java/com/example/handlers/ApiHandler.java
+++ calculator-backend/src/main/java/com/example/handlers/ApiHandler.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 import com.example.models.DataModel;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class ApiHandler implements HttpHandler {
     @Override
@@ -15,17 +16,27 @@ public class ApiHandler implements HttpHandler {
         Gson gson = new Gson();
         JsonParserClass jsonParser = new JsonParserClass();
 
-        //  Handle POST request
+        // Handle POST request
         if ("POST".equals(exchange.getRequestMethod())) {
             Scanner scanner = new Scanner(exchange.getRequestBody());
             StringBuilder requestBody = new StringBuilder();
-            while (scanner.hasNext())
-                requestBody.append(scanner.nextLine());
+            while (scanner.hasNext()) requestBody.append(scanner.nextLine());
             scanner.close();
 
             System.out.println("Request Body: " + requestBody.toString());
 
-            DataModel receivedData = jsonParser.parseJson(requestBody.toString());
+            DataModel receivedData = null;
+            try {
+                receivedData = jsonParser.parseJson(requestBody.toString());
+            } catch (JsonSyntaxException e) {
+                String errorMessage = "Invalid JSON format";
+                exchange.sendResponseHeaders(400, errorMessage.getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(errorMessage.getBytes());
+                os.close();
+                return;
+            }
+
             String response = gson.toJson(receivedData);
             System.out.println(response);
 
@@ -35,23 +46,22 @@ public class ApiHandler implements HttpHandler {
             exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "content-type");
             exchange.sendResponseHeaders(200, response.getBytes().length);
 
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
         }
 
-        //  Handle other request
+        // Handle OPTIONS request
         else if ("OPTIONS".equals(exchange.getRequestMethod())) {
             exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
             exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
             exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
             exchange.sendResponseHeaders(204, -1); // No Content
-        } 
+        }
 
-        //  Handle unsupported request
+        // Handle unsupported request method
         else {
-            exchange.sendResponseHeaders(405, -1);
+            exchange.sendResponseHeaders(405, -1); // Method Not Allowed
         }
     }
-    
 }
